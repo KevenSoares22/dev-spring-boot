0de8bca2e6f4f6e3c37cbdd5a6e2b93fcf483d8e
KevenSoares22 <keven.v.soares@gmail.com>
2024-07-15 22:18:15 -0300

    Init

--- mynewapp/src/main/java/com/spark/dev/mynewapp/MynewappApplication.java
+++ mynewapp/src/main/java/com/spark/dev/mynewapp/MynewappApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MynewappApplication {
 
+
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(MynewappApplication.class, args);
 	}
